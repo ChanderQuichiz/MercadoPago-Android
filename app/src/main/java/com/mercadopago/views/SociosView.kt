3ac31be2601c33e1312d5fead2ba9a74245609e4
Seb24AND <sebasalegriaq@gmail.com>
2026-06-07 20:12:45 -0500

    Socios Modificado y con Funcionalidad

--- app/src/main/java/com/mercadopago/views/SociosView.kt
+++ app/src/main/java/com/mercadopago/views/SociosView.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -14,18 +13,33 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.mercadopago.R
 import com.mercadopago.models.SocioModel
-
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.clickable
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.material3.HorizontalDivider
 @Composable
 fun SociosView(
     navController: NavController
 ) {
 
     DetailedDrawer(navController = navController) { padding ->
+
+        var filtroExpandido by remember { mutableStateOf(false) }
+
+        val opcionesEstado = listOf(
+            "Todos",
+            "Activo",
+            "Inactivo"
+        )
+
+        var estadoSeleccionado by remember {
+            mutableStateOf("Todos")
+        }
 
         var busqueda by remember {
             mutableStateOf("")
@@ -74,20 +88,139 @@ fun SociosView(
 
             Spacer(modifier = Modifier.height(15.dp))
 
-            OutlinedTextField(
-                value = busqueda,
-                onValueChange = {
-                    busqueda = it
-                },
-                modifier = Modifier.fillMaxWidth(),
-                placeholder = {
-                    Text("Buscar por DNI o nombre...")
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        Color.White,
+                        RoundedCornerShape(15.dp)
+                    )
+                    .padding(15.dp)
+            ) {
+
+                OutlinedTextField(
+                    value = busqueda,
+                    onValueChange = {
+                        busqueda = it
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = {
+                        Text("Buscar por DNI o nombre...")
+                    }
+                )
+
+                Spacer(modifier = Modifier.height(12.dp))
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            Color.White,
+                            RoundedCornerShape(12.dp)
+                        )
+                        .border(
+                            1.dp,
+                            Color.LightGray,
+                            RoundedCornerShape(12.dp)
+                        )
+                )
+                {
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                filtroExpandido = !filtroExpandido
+                            }
+                            .padding(16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+                        Text(
+                            text = estadoSeleccionado,
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.Center,
+                            color = Color.Black,
+                            fontFamily = FontFamily(
+                                Font(R.font.changa_medium)
+                            )
+                        )
+
+                        Text(
+                            text =
+                                if (filtroExpandido)
+                                    "˄"
+                                else
+                                    "˅",
+                            color = Color.Black
+                        )
+                    }
+
+                    if (filtroExpandido) {
+
+                        HorizontalDivider()
+
+                        listOf(
+                            "Todos",
+                            "Activo",
+                            "Inactivo"
+                        ).forEach { opcion ->
+
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clickable {
+
+                                        estadoSeleccionado = opcion
+                                        filtroExpandido = false
+                                    }
+                                    .padding(16.dp),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+
+                                Text(
+                                    opcion,
+                                    color =
+                                        if (opcion == estadoSeleccionado)
+                                            Color(0xFF355CC0)
+                                        else
+                                            Color.Black
+                                )
+                            }
+
+                            if (opcion != "Inactivo") {
+                                HorizontalDivider()
+                            }
+                        }
+                    }
                 }
-            )
+            }
 
             Spacer(modifier = Modifier.height(20.dp))
 
-            socios.forEach { socio ->
+            val sociosFiltrados = socios.filter { socio ->
+
+                val coincideBusqueda =
+                    busqueda.isBlank() ||
+                            socio.nombre.contains(
+                                busqueda,
+                                ignoreCase = true
+                            ) ||
+                            socio.dni.contains(
+                                busqueda,
+                                ignoreCase = true
+                            )
+
+                val coincideEstado =
+                    estadoSeleccionado == "Todos" ||
+                            socio.estado.equals(
+                                estadoSeleccionado,
+                                ignoreCase = true
+                            )
+
+                coincideBusqueda && coincideEstado
+            }
+            sociosFiltrados.forEach { socio ->
 
                 Column(
                     modifier = Modifier
@@ -102,132 +235,134 @@ fun SociosView(
                             Color(0xFFD1D1D1),
                             RoundedCornerShape(15.dp)
                         )
-                        .padding(20.dp)
+                        .padding(15.dp)
                 ) {
 
-                    Text(
-                        "Socio",
-                        color = Color.Black,
-                        fontFamily = FontFamily(
-                            Font(R.font.inclusivesans_variablefont_wght)
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+
+                        // FOTO DEL USUARIO
+                        Box(
+                            modifier = Modifier
+                                .width(75.dp)
+                                .height(100.dp)
+                                .align(Alignment.CenterVertically)
+                                .background(
+                                    Color(0xFFD9D9D9),
+                                    RoundedCornerShape(4.dp)
+                                )
+                                .border(
+                                    1.dp,
+                                    Color.LightGray,
+                                    RoundedCornerShape(4.dp)
+                                )
                         )
-                    )
 
-                    Spacer(modifier = Modifier.height(5.dp))
+                        Spacer(modifier = Modifier.width(15.dp))
 
-                    Text(
-                        socio.nombre,
-                        color = Color.Black,
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center,
-                        fontFamily = FontFamily(
-                            Font(R.font.changa_medium)
-                        )
+                        Column(
+                            modifier = Modifier.weight(1f)
+                        ) {
 
-                    )
+                            // ESTADO
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.End,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
 
-                    HorizontalDivider(
-                        modifier = Modifier.padding(vertical = 10.dp),
-                        color = Color.LightGray
-                    )
-                    Spacer(modifier = Modifier.height(20.dp))
+                                Box(
+                                    modifier = Modifier
+                                        .size(12.dp)
+                                        .background(
+                                            if (socio.estado == "ACTIVO")
+                                                Color(0xFF55C5B8)
+                                            else
+                                                Color.Red,
+                                            CircleShape
+                                        )
+                                )
 
-                    Text(
-                        "DNI",
-                        color = Color.Black,
-                        fontFamily = FontFamily(
-                            Font(R.font.inclusivesans_variablefont_wght)
-                        )
-                    )
+                                Spacer(modifier = Modifier.width(6.dp))
 
-                    Spacer(modifier = Modifier.height(5.dp))
+                                Text(
+                                    text = socio.estado,
+                                    color =
+                                        if (socio.estado == "ACTIVO")
+                                            Color(0xFF00E539)
+                                        else
+                                            Color.Red,
+                                    fontFamily = FontFamily(
+                                        Font(R.font.changa_medium)
+                                    )
+                                )
+                            }
 
-                    Text(
-                        socio.dni,
-                        color = Color.Black,
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center
-                    )
+                            Spacer(modifier = Modifier.height(10.dp))
 
-                    HorizontalDivider(
-                        modifier = Modifier.padding(vertical = 10.dp),
-                        color = Color.LightGray
-                    )
-                    Spacer(modifier = Modifier.height(20.dp))
+                            // DNI Y TELEFONO
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
 
-                    Text(
-                        "Contacto",
-                        color = Color.Black,
-                        fontFamily = FontFamily(
-                            Font(R.font.inclusivesans_variablefont_wght)
-                        )
-                    )
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
 
-                    Spacer(modifier = Modifier.height(5.dp))
+                                    Text(
+                                        "DNI",
+                                        color = Color.Black,
+                                        fontFamily = FontFamily(
+                                            Font(R.font.changa_medium)
+                                        )
+                                    )
 
-                    Text(
-                        socio.correo,
-                        color = Color.Black,
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center
-                    )
+                                    Text(
+                                        socio.dni,
+                                        color = Color.Black
+                                    )
+                                }
 
-                    Text(
-                        socio.telefono,
-                        color = Color.Black,
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center
-                    )
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
 
-                    HorizontalDivider(
-                        modifier = Modifier.padding(vertical = 10.dp),
-                        color = Color.LightGray
-                    )
-                    Spacer(modifier = Modifier.height(20.dp))
+                                    Text(
+                                        "Telefono",
+                                        color = Color.Black,
+                                        fontFamily = FontFamily(
+                                            Font(R.font.changa_medium)
+                                        )
+                                    )
 
-                    Text(
-                        "Rol",
-                        color = Color.Black,
-                        fontFamily = FontFamily(
-                            Font(R.font.inclusivesans_variablefont_wght)
-                        )
-                    )
+                                    Text(
+                                        socio.telefono,
+                                        color = Color.Black
+                                    )
+                                }
+                            }
 
-                    Spacer(modifier = Modifier.height(5.dp))
+                            Spacer(modifier = Modifier.height(15.dp))
 
-                    Text(
-                        socio.rol,
-                        color = Color.Black,
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center
-                    )
+                            // CONTACTO
+                            Text(
+                                "Contacto",
+                                color = Color.Gray,
+                                fontFamily = FontFamily(
+                                    Font(R.font.changa_medium)
+                                )
+                            )
 
-                    HorizontalDivider(
-                        modifier = Modifier.padding(vertical = 10.dp),
-                        color = Color.LightGray
-                    )
-                    Spacer(modifier = Modifier.height(20.dp))
+                            Spacer(modifier = Modifier.height(5.dp))
 
-                    Text(
-                        "Estado",
-                        color = Color.Black,
-                        fontFamily = FontFamily(
-                            Font(R.font.inclusivesans_variablefont_wght)
-                        )
-                    )
-
-                    Spacer(modifier = Modifier.height(5.dp))
-
-                    Text(
-                        socio.estado,
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center,
-                        color =
-                            if (socio.estado == "ACTIVO")
-                                Color(0xFF32CD32)
-                            else
-                                Color.Red
-                    )
+                            Text(
+                                socio.correo,
+                                color = Color.Black
+                            )
+                        }
+                    }
                 }
             }
         }
